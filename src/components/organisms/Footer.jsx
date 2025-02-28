943fee1e843bf1c28de6608fd83eaa3f7b11ef9f
muamarzidan <codeofomiru11@gmail.com>
2025-02-28 15:42:59 +0700

    test

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 text-center">
-                        {/* <script>{new Date().getFullYear()}</script> &copy; Admin by Digivise. */}
+                        <script>{new Date().getFullYear()}</script> &copy; Admin by Digivise.
                     </div>
                 </div>
             </div>
