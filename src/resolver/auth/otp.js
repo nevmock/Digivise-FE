d2de045fc411b5a3409009f639176458ab5867eb
muamarzidan <codeofomiru11@gmail.com>
2025-05-03 09:48:09 +0700

    feat: merchant feature

--- src/resolver/auth/otp.js
+++ src/resolver/auth/otp.js
@@ -1,7 +1,7 @@
 import axiosRequest from "../../utils/request";
 const API_URL = import.meta.env.BE_API_URL;
 
-export default async function verifyOtp(otp) {
+export async function verifyOtp(otp) {
     try {
         const response = await axiosRequest.post(`${API_URL}/v1/verify-otp`, { otp });
         return response.data;
