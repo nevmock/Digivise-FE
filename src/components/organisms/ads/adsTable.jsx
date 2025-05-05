6d4131fb45e67024615b9eb1f7ebe8e423f47198
muamarzidan <codeofomiru11@gmail.com>
2025-05-05 23:32:29 +0700

    fix: route detail

--- src/components/organisms/ads/adsTable.jsx
+++ src/components/organisms/ads/adsTable.jsx
@@ -1456,18 +1456,18 @@ const AdsTable = ({ data }) => {
                           <td style={{ width: "200px" }}>
                             {
                               entry.type === "product_gmv_max_roas" ? (
-                                <Link to={"/performance/ads/detailROAS"}>
+                                <Link to={"/dashboard/performance/ads/detailROAS"}>
                                   roas
                                   <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="m16 8.4l-8.9 8.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7L14.6 7H7q-.425 0-.712-.288T6 6t.288-.712T7 5h10q.425 0 .713.288T18 6v10q0 .425-.288.713T17 17t-.712-.288T16 16z"></path></svg>
                                 </Link>
                               ) : entry.type === "product_manual" ? (
                                 entry?.manual_product_ads?.product_placement === "search_product" ? (
-                                  <Link to={"/performance/ads/detail"}>
+                                  <Link to={"/dashboard/performance/ads/detail"}>
                                     se
                                     <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="m16 8.4l-8.9 8.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7L14.6 7H7q-.425 0-.712-.288T6 6t.288-.712T7 5h10q.425 0 .713.288T18 6v10q0 .425-.288.713T17 17t-.712-.288T16 16z"></path></svg>
                                   </Link>
                                 ) : entry?.manual_product_ads?.product_placement === "targeting" ? (
-                                  <Link to={"/performance/ads/detailRECOM"}>
+                                  <Link to={"/dashboard/performance/ads/detailRECOM"}>
                                     re
                                     <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="m16 8.4l-8.9 8.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7L14.6 7H7q-.425 0-.712-.288T6 6t.288-.712T7 5h10q.425 0 .713.288T18 6v10q0 .425-.288.713T17 17t-.712-.288T16 16z"></path></svg>
                                   </Link>
