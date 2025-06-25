4b38409abdae863c8cfe0cb20751f9a5f74d0ca8
muamarzidan <codeofomiru11@gmail.com>
2025-06-25 17:30:57 +0700

    fix: undefined import

---
+++ src/utils/convertFormatToENG.js
@@ -0,0 +1,15 @@
+const convertFormatEnglish = (value) => {
+    const newValue = Number(value);
+
+    if (newValue >= 1_000_000_000) {
+        return (newValue / 1_000_000_000).toFixed(1).replace(/\.0$/, '') + 'b';
+    } else if (newValue >= 1_000_000) {
+        return (newValue / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'm';
+    } else if (newValue >= 1_000) {
+        return (newValue / 1_000).toFixed(1).replace(/\.0$/, '') + 'k';
+    } else {
+        return newValue.toString();
+    }
+}
+
+export default convertFormatEnglish;
